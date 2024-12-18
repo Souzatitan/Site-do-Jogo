d42356b73ad422aef138145a3bb537e462379f2e
Souzatitan <asouzanderson@gmail.com>
2024-12-17 22:28:30 -0300

    commit2

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+// Seleciona os elementos
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+// Adiciona o evento de clique
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active'); // Adiciona ou remove a classe "active"
+});
